751fee46df88a7cd526660594655bf54d2e376b1
Daniel Larkin <realdanieljlarkin@gmail.com>
2024-03-26 11:38:02 -0500

    completed file tree

---
+++ src/components/Tree.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { useState } from "react";
+
+interface Level {
+  name: string;
+  children?: Level[];
+}
+
+const fileTree: Level[] = [
+  {
+    name: "folder1",
+    children: [
+      {
+        name: "folder1b",
+        children: [
+          {
+            name: "file1b",
+          },
+          {
+            name: "file2b",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "folder2",
+    children: [
+      {
+        name: "file2a",
+      },
+      {
+        name: "file2b",
+      },
+      {
+        name: "folder2b",
+        children: [
+          {
+            name: "file1b",
+          },
+          {
+            name: "file2b",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "folder3",
+    children: [
+      {
+        name: "file3a",
+      },
+      {
+        name: "file3b",
+      },
+      {
+        name: "folder3b",
+        children: [
+          {
+            name: "file1b",
+          },
+          {
+            name: "file2b",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "folder4",
+    children: [
+      {
+        name: "file3a",
+      },
+      {
+        name: "file3b",
+      },
+    ],
+  },
+  {
+    name: "file1",
+  },
+];
+
+const Level = ({ level, indent }: { level: Level; indent: number }) => {
+  const [openTree, setOpenTree] = useState(false);
+  return (
+    <div className=" w-[500px] overflow-hidden">
+      <div className={`flex items-center gap-1 `}>
+        {level.children && (
+          <button onClick={() => setOpenTree(!openTree)}>
+            {!openTree ? "+" : "-"}
+          </button>
+        )}
+        <h2>{level.name}</h2>
+      </div>
+      {openTree && (
+        <div>
+          {level.children?.map((child) => (
+            <div className={`pl-[25px]`}>
+              <Level level={child} indent={indent + 10} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Tree = () => {
+  return (
+    <div>
+      {fileTree.map((level) => (
+        <Level level={level} indent={0} />
+      ))}
+    </div>
+  );
+};
+
+export default Tree;
